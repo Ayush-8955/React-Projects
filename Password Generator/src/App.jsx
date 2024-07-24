a075302fc939256cec5e7870106cf3ddd9ddc817
Ayush-8955 <ayush058955@gmail.com>
2024-07-24 23:37:07 +0530

    chnages in password generator project , adding new button , improving previous code of generating characters

--- Password Generator/src/App.jsx
+++ Password Generator/src/App.jsx
@@ -4,24 +4,73 @@ import { useState ,useCallback,useEffect, useRef} from 'react'
 function App() {
   const [length,setLength]=useState(8)
   const [numAllowed,setNumAllowed]=useState(false)
+
   const [charAllowed,setCharAllowed]=useState(false)
   const [password,setPassword]=useState('')
 
   // useRef hook
   const passwordRef=useRef(null)
 
+  // to shuffle the string we use this :
+
+  function shuffleString(str) {
+    // Convert the string to an array
+    let arr = str.split('');
+  
+    // Shuffle the array using the **Fisher-Yates algorithm **
+    for (let i = arr.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+  
+    // Convert the shuffled array back to a string
+    return arr.join('');
+  }
+
   const passwordGenerator=useCallback(()=>{
     let pass='';
     let str='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
-    if (numAllowed) str += "0123456789"
-
-    if (charAllowed) str += "!@#$%^&*-_+=~"
-
-    for(let i=0;i<=length;i++){
-      let ind=Math.floor(Math.random()*str.length+1)
-      pass+=str.charAt(ind)
-      setPassword(pass)
+    let len=length;
+    if (numAllowed){ 
+      // str += "0123456789"
+      let numStr="0123456789"
+      let numLen=Math.floor(Math.random()*(length/3)+1)
+      // console.log("numLen is " ,numLen)
+      for(let i=1;i<=numLen;i++){
+        let ind=Math.floor(Math.random()*numStr.length)
+        pass+=numStr.charAt(ind)
+        //  setPassword(pass)
+      }
+      len=len-numLen
+      // console.log("len is ",len)
     }
+    if (charAllowed){ 
+      let charStr = "!@#$%^&*-_+=~"
+      let charLen=Math.floor(Math.random()*(length/3)+1)
+      // console.log('charLen is ',charLen)
+      for(let i=1;i<=charLen;i++){
+        let ind=Math.floor(Math.random()*charStr.length)
+        pass+=charStr.charAt(ind)
+        //  setPassword(pass)
+      }
+      len=len-charLen
+      // console.log('len is ',len)
+
+    }
+    // console.log("final len is ",len)
+
+    for(let i=1;i<=len;i++){
+      let ind=Math.floor(Math.random()*str.length)
+      pass+=str.charAt(ind)
+      //  setPassword(pass)
+       
+    }
+    // setPassword((prev)=>shuffleString(prev))
+    setPassword(shuffleString(pass))
+
+    //here problem is that there is order in password that first digits then characters and then other values so we have to shuffle this
+
+
   },[length,numAllowed,charAllowed,setPassword]) // array ->values pass such that -> optimization,memonization
 
   const copyPasswordToClipboard=useCallback(()=>{
@@ -47,7 +96,7 @@ useEffect(()=>{
     <div className="flex shadow rounded-lg overflow-hidden mb-4">
       <input 
       type="text"
-      value={password}
+      value={password}// when page relaod first empty value then as useEffect already call (at reloads) then there some password value generate which is shown (this very fast that we don't see empty )
       className='outline-none w-full py-1 px-3'
       placeholder='Password'
       readOnly
@@ -55,7 +104,7 @@ useEffect(()=>{
       />
       <button  
       onClick={copyPasswordToClipboard}
-        className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'>
+        className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0 hover:bg-blue-600'>
         Copy
       </button>
     </div>
@@ -95,6 +144,9 @@ useEffect(()=>{
               }}
           />
           <label htmlFor="characterInput">Characters</label>
+      </div>
+      <div>
+        <button className=' bg-red-500 text-white hover:bg-red-400' onClick={passwordGenerator}  style={{width:'50px' ,borderRadius:'5px' , marginLeft:'10px'}}>New</button>
       </div>
     </div>
 
